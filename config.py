042ef92dbe7e0057c76262af1f87b2a43c9315cf
Moaz <32312009+MOAZ47@users.noreply.github.com>
2025-05-29 06:59:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
-WEAVIATE_URL = os.getenv("WEAVIATE_URL", "qsgmkeaskuouflhwsx8ra.c0.asia-southeast1.gcp.weaviate.cloud")
-WEAVIATE_API_KEY = os.getenv("WEAVIATE_API_KEY", "td5y9thNzdjURYoUW6fCwlXIYxgUqOg4ddwm")
-COHERE_API_KEY = os.getenv("COHERE_API_KEY", "F7aq79uHk5nq5vStWOxGDE6nGB5h545DIua5gJrV")
+WEAVIATE_URL = os.getenv("WEAVIATE_URL", "")
+WEAVIATE_API_KEY = os.getenv("WEAVIATE_API_KEY", "")
+COHERE_API_KEY = os.getenv("COHERE_API_KEY", "")
 AUDIO_FILE_PATH = "elon_musk_starship_rocket.mp3"
 CHUNK_SIZE = 500
 CHUNK_OVERLAP = 50
